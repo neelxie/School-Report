64528ab8299c5a5ca94069124e501d375b424015
neelxie <kidricederek@gmail.com>
2023-12-02 19:34:10 +0300

    Add endpoints for individual languagequestions

--- app/web_routes.py
+++ app/web_routes.py
@@ -116,6 +116,60 @@ def main_rank():
         sub_category=sub_category,
     )
 
+@web_bp.route("/expert_english")
+def expert_english():
+    access_token = session.get("token")
+    role = session.get("role")
+    username = session.get("username")
+    language = session.get("language")
+    category = session.get("category")
+    sub_category = session.get("sub_category")
+    return render_template(
+        "expertEnglish.html",
+        token=access_token,
+        role=role,
+        user=username,
+        language=language,
+        category=category,
+        sub_category=sub_category,
+    )
+
+@web_bp.route("/expert_luganda")
+def expert_luganda():
+    access_token = session.get("token")
+    role = session.get("role")
+    username = session.get("username")
+    language = session.get("language")
+    category = session.get("category")
+    sub_category = session.get("sub_category")
+    return render_template(
+        "expertLuganda.html",
+        token=access_token,
+        role=role,
+        user=username,
+        language=language,
+        category=category,
+        sub_category=sub_category,
+    )
+
+@web_bp.route("/expert_runyankole")
+def expert_runyankole():
+    access_token = session.get("token")
+    role = session.get("role")
+    username = session.get("username")
+    language = session.get("language")
+    category = session.get("category")
+    sub_category = session.get("sub_category")
+    return render_template(
+        "expertRunyankole.html",
+        token=access_token,
+        role=role,
+        user=username,
+        language=language,
+        category=category,
+        sub_category=sub_category,
+    )
+
 
 @web_bp.route("/answer_page.html")
 def answer_page():
